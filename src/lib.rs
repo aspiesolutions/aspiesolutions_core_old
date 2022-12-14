acc973bc66847a88493861ff1af27e1ff96c80cc
Jordan Morris <jthecybertinkerer@gmail.com>
2022-12-14 16:55:36 -0600

    update types to require sending optional data along with errors

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@
 pub mod rocket;
 
 
-type Results<T,E> = Vec<Result<T,E>>;
+type Results<T,E> = Vec<Result<T,ErrorWithOptionalData<T,E>>>; 
+type ErrorsWithOptionalData<T,E> = Vec<ErrorWithOptionalData<T,E>>;
 
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -50,17 +51,46 @@ pub type PagedResult<T, E> = Result<Paged<T>, E>;
 #[derive(PartialEq, Eq)]
 pub struct Recoverable<T, E> {
     data: Vec<T>,
-    errors: Vec<E>,
+    errors: ErrorsWithOptionalData<T,E>,
+}
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "debug", derive(Debug))]
+#[cfg_attr(feature = "clone", derive(Clone))]
+#[derive(PartialEq, Eq)]
+
+pub struct ErrorWithOptionalData<T,E> {
+    data:Option<T>,
+    error:E
+}
+
+impl<T,E> std::convert::From<(Option<T>,E)> for ErrorWithOptionalData<T,E> {
+    fn from((data,error): (Option<T>,E)) -> Self {
+        Self{data,error}
+    }
+}
+
+
+impl<'a,'b,T,E> ErrorWithOptionalData<T,E> {
+    pub fn new(data:Option<T>,error:E) -> Self {
+        Self { data, error }
+    }
+    pub fn data(&'a self) -> Option<&'a T> {
+        self.data.as_ref()
+    }
+    pub fn error(&'b self) -> &'b E {
+        &self.error
+    }
+
 }
 
 impl<T,E> Recoverable<T,E> {
-    pub fn new(data:Vec<T>,errors:Vec<E>) -> Self {
+    pub fn new(data:Vec<T>,errors: ErrorsWithOptionalData<T,E>) -> Self {
         Self{data,errors}
     }
     pub fn data<'a>(&'a self) -> &'a Vec<T> {
         self.data.as_ref()
     }
-    pub fn errors<'a>(&'a self) -> &'a Vec<E> {
+    pub fn errors<'a>(&'a self) -> &'a ErrorsWithOptionalData<T,E>{
         self.errors.as_ref()
     }
 }
@@ -68,7 +98,7 @@ impl<T,E> Recoverable<T,E> {
 impl<T,E> std::convert::From<Results<T,E>> for Recoverable<T,E> {
     fn from(results: Results<T,E>) -> Self {
         let mut data = Vec::<T>::new();
-        let mut errors = Vec::<E>::new();
+        let mut errors = Vec::<ErrorWithOptionalData<T,E>>::new();
         for result in results {
             match result {
                 Ok(t) => data.push(t),
@@ -82,19 +112,20 @@ impl<T,E> std::convert::From<Results<T,E>> for Recoverable<T,E> {
         
     }
 }
-impl<T,E> std::convert::From<(T,Vec<E>)> for Recoverable<T,E> {
-    fn from((t,errors): (T,Vec<E>)) -> Self {
+impl<T,E> std::convert::From<(T,ErrorsWithOptionalData<T,E>)> for Recoverable<T,E> {
+    fn from((t,errors): (T,ErrorsWithOptionalData<T,E>)) -> Self {
         Self {
             data:vec![t],
             errors
         }
     }
 }
-impl<T,E> std::convert::From<(Vec<T>,Vec<E>)> for Recoverable<T,E> {
-    fn from((data,errors): (Vec<T>,Vec<E>)) -> Self {
+impl<T,E> std::convert::From<(Vec<T>,ErrorsWithOptionalData<T,E>)> for Recoverable<T,E> {
+    fn from((data,errors): (Vec<T>,ErrorsWithOptionalData<T,E>)) -> Self {
         Self {
             data,
             errors
         }
     }
 }
+type MaybeRecoverable<T,E> = Result<Recoverable<T,E>,E>;
