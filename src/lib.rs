f5c7337d45c7e03a8d0b32d714be79371f3b1930
Jordan Morris <jthecybertinkerer@gmail.com>
2022-12-14 14:03:07 -0600

    working on types

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,20 @@ pub mod rocket;
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[derive(PartialEq, Eq)]
-pub struct PagedResponse<T> {
+pub struct Paged<T> {
     num_items: u64,
     num_pages: u64,
     cur_page: u64,
     data: Vec<T>,
 }
 
-pub type PagedResponseResult<T, E> = Result<PagedResponse<T>, E>;
+pub type PagedResult<T, E> = Result<Paged<T>, E>;
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[derive(PartialEq, Eq)]
-pub struct BatchOperationResponse<T, E> {
+pub struct Recoverable<T, E> {
     data: Vec<T>,
     errors: Vec<E>,
 }
