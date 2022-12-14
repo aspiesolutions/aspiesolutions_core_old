97e5e761efa6a80a5be5e47a30a7a1793079a665
Jordan Morris <jthecybertinkerer@gmail.com>
2022-12-14 14:21:59 -0600

    add accessors

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,28 @@ pub struct Paged<T> {
     cur_page: u64,
     data: Vec<T>,
 }
-
+impl<'a,'b,'c,'d,T> Paged<T> {
+    pub fn new(num_items:u64,num_pages:u64,cur_page:u64,data:Vec<T>) -> Self {
+        Self {
+            num_items,
+            num_pages,
+            cur_page,
+            data
+        }
+    }
+    pub fn num_items(&'a self) -> &'a u64 {
+        &self.num_items
+    }
+    pub fn num_pages(&'b self) -> &'b u64 {
+        &self.num_pages
+    }
+    pub fn cur_page(&'c self) -> &'c u64 {
+        &self.cur_page
+    }
+    pub fn data(&'d self) -> &'d Vec<T> {
+        self.data.as_ref()
+    }
+}
 pub type PagedResult<T, E> = Result<Paged<T>, E>;
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -26,4 +47,16 @@ pub type PagedResult<T, E> = Result<Paged<T>, E>;
 pub struct Recoverable<T, E> {
     data: Vec<T>,
     errors: Vec<E>,
+}
+
+impl<T,E> Recoverable<T,E> {
+    pub fn new(data:Vec<T>,errors:Vec<E>) -> Self {
+        Self{data,errors}
+    }
+    pub fn data<'a>(&'a self) -> &'a Vec<T> {
+        self.data.as_ref()
+    }
+    pub fn errors<'a>(&'a self) -> &'a Vec<E> {
+        self.errors.as_ref()
+    }
 }
